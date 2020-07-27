645a8f0f5dd5c1f48974ef957995b903bbfe83ee
cortejon <cortejon@cs.washington.edu>
2020-07-27 10:03:27 -0700

    Added assets and created ball class and prefab. Also added ball type enum

---
+++ Pool/Assets/Resources/Scripts/Ball.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    private SpriteRenderer sr;
+    private Animator anim;
+
+    [SerializeField] int number;
+    private BallType type;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sr = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
+
+        type = number < 7  ? BallType.SOLID :
+               number == 7 ? BallType.EIGHT :
+                             BallType.STRIPE;  // number > 7
+
+        anim.SetInteger("number", number);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
