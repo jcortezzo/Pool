3d2bedfaae295adf5c55884aba7db414820a0ea2
cortejon <cortejon@cs.washington.edu>
2020-07-27 10:25:49 -0700

    Ball basics implemented and finished. Yet to add player interaction

--- Pool/Assets/Resources/Scripts/Ball.cs
+++ Pool/Assets/Resources/Scripts/Ball.cs
@@ -6,26 +6,47 @@ public class Ball : MonoBehaviour
 {
     private SpriteRenderer sr;
     private Animator anim;
+    private Rigidbody2D rb;
 
     [SerializeField] int number;
     private BallType type;
+
+    private const int CUE_NUMBER = 16;
+
+    [Header("Debug Control")]
+    [SerializeField] private bool control;
+    [SerializeField] private float speed;
 
     // Start is called before the first frame update
     void Start()
     {
         sr = GetComponent<SpriteRenderer>();
         anim = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
 
         type = number < 7  ? BallType.SOLID :
                number == 7 ? BallType.EIGHT :
                              BallType.STRIPE;  // number > 7
 
-        anim.SetInteger("number", number);
+        anim.Play("" + number);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void FixedUpdate()
+    {
+        if (control) Move();
+    }
+
+    void Move()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical   = Input.GetAxisRaw("Vertical");
+
+        rb.velocity = new Vector2(horizontal, vertical).normalized * Time.deltaTime * speed;
     }
 }
