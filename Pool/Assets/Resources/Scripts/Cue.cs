ec00c03e9f2c3a90e32073078efc500045cab173
cortejon <cortejon@cs.washington.edu>
2020-07-27 20:14:45 -0700

    added line from cue tip to ball. Toggle it on or off with lshift

--- Pool/Assets/Resources/Scripts/Cue.cs
+++ Pool/Assets/Resources/Scripts/Cue.cs
@@ -7,8 +7,13 @@ public class Cue : MonoBehaviour
     private Rigidbody2D rb;
     private BoxCollider2D col;
     private SpriteRenderer sr;
+    private Transform tip;
+    private LineRenderer lr;
+    [SerializeField] private LayerMask layerMask;
 
     private bool isActive;
+    private bool showLine;
+    private Vector2 direction;
 
     // Start is called before the first frame update
     void Start()
@@ -16,6 +21,17 @@ public class Cue : MonoBehaviour
         rb = GetComponent<Rigidbody2D>();
         col = GetComponent<BoxCollider2D>();
         sr = GetComponent<SpriteRenderer>();
+        tip = GetComponentInChildren<Transform>();  // ?
+        lr = GetComponent<LineRenderer>();
+
+        lr.SetPosition(0, tip.position);
+        lr.SetPosition(1, tip.position);
+        lr.startColor = Color.blue;
+        lr.endColor = Color.blue;
+        lr.startWidth = 0.1f;
+        lr.endWidth = 0.1f;
+        showLine = true;
+
         col.isTrigger = true;
         Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         transform.position = new Vector3(mousePos.x, mousePos.y, transform.position.z);
@@ -27,6 +43,29 @@ public class Cue : MonoBehaviour
         isActive = Input.GetMouseButton(0);
         col.isTrigger = !isActive;
         sr.color = isActive ? Color.white : Color.gray;
+
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            showLine = !showLine;
+            lr.enabled = false;
+        }
+
+        if (showLine)
+        {
+            lr.SetPosition(0, tip.position);
+            RaycastHit2D hit = Physics2D.Raycast(tip.position, -transform.right,
+                                                 100000f, layerMask);
+            Debug.DrawRay(tip.position, -transform.right * 10000f, Color.white, 0.1f, false);
+            if (hit.collider != null)
+            {
+                lr.enabled = true;
+                lr.SetPosition(1, hit.point);
+            }
+            else
+            {
+                lr.enabled = false;
+            }
+        }
     }
     
     void FixedUpdate()
@@ -59,5 +98,7 @@ public class Cue : MonoBehaviour
             rb.MoveRotation(Mathf.Rad2Deg * angle);
         }
         rb.angularVelocity = 0f;
+
+        direction = transform.right;//mousePos - tip.position;//transform.eulerAngles;
     }
 }
